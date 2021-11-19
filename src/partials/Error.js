7038b5b8fbedd3797873a7f4254dcd70cfde2c95
AlinaBaliuk <baliuk.asua@gmail.com>
2021-11-19 22:53:34 +0200

    fix

--- src/partials/Error.js
+++ src/partials/Error.js
@@ -4,10 +4,8 @@ const Error = () => {
 	return (
 		<div>
 			<h1>404</h1>
-			<h2>UH OH! You're lost.</h2>
 			<p>
-				The page you are looking for does not exist. How you got here is a
-				mystery.
+				The page you are looking for does not exist.
 			</p>
 		</div>
 	);
